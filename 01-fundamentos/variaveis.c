c3e6cd53d19e5baebdae6203cceae0ba5c0075a8
soraiacavato <soraiacavato@gmail.com>
2025-08-28 18:27:16 +0000

    //Atualizado

---
+++ 01-fundamentos/variaveis.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    //variáveis
+
+    char nome[50] = "";
+    int idade = 0;
+    float altura = 1.0;
+
+    //perguntas
+
+    printf ("Insira o seu nome: ");
+    scanf ("%s", nome);
+
+    printf ("Insira a sua idade: ");
+    scanf ("%d", &idade);
+
+    printf ("Insira a sua altura: ");
+    scanf ("%f", &altura);
+
+    //respostas
+
+    printf ("Seu nome e: %s.\n", nome);
+    printf ("Sua idade e: %d.\n", idade);
+    printf ("Sua altura e: %f.\n", altura);
+
+
+    system("pause");
+    return 0;
+}
