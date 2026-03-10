66eede06d3dd971881fd00eb17e9aaacea06fc3d
Soraia Cavato <soraiacavato@gmail.com>
2026-03-09 22:45:51 -0300

    cria o arquivo soma-funcao.c

--- 02-funcoes-e-estruturas/1-funcoes/soma-funcao.c
+++ 02-funcoes-e-estruturas/1-funcoes/soma-funcao.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int soma (int a, int b) {
+    return a + b;
+}
+
+int main () {
+
+    int num1, num2, resultado;
+
+    printf ("========== Calculadora de Soma ==========\n\n");
+
+    printf ("Digite o primeiro numero: ");
+    scanf ("%d", &num1);
+
+    printf ("\nDigite o segundo numero: ");
+    scanf ("%d", &num2);
+
+    printf ("\n\n");
+
+    printf ("A soma de %d + %d = %d\n\n", num1, num2, soma(num1, num2));
+
+
+    system ("pause");
+    return 0;
+}
